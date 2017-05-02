63fa105d5d2cf0bd128982f2a9cb2b505e1fc646
Mujtaba Aslam <aslammut@cs.grinnell.edu>
2017-05-01 21:55:48 -0500

    commented and finished

--- src/Grin.java
+++ src/Grin.java
@@ -3,48 +3,72 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Grin {
-    public static void decode(String infile, String outfile) throws IOException{
-        BitInputStream in = new BitInputStream(infile);
-        BitOutputStream out = new BitOutputStream(outfile);
-        int magic = in.readBits(32);
-        if (magic != 1846) {
-            throw new IllegalArgumentException("Not a .grin file");
-        }
-        HuffmanTree tree = new HuffmanTree(in);
-        tree.decode(in, out);
-    }
-    
-   public static Map<Short, Integer> createFrequencyMap(String file) throws IOException{
-       Map<Short, Integer> m = new HashMap<>();
-       BitInputStream in = new BitInputStream(file);
-     while (in.hasBits()){
-         int temp = in.readBits(8);
-         if(m.containsKey((short) temp)){
-             m.put((short)temp, m.get((short) temp) + 1);
-         }
-         else{
-             m.put((short)temp, 1);
-         }
-     }
-     in.close();
-       return m; 
-    }
-   
-   public static void encode(String infile, String outfile) throws IOException{
-       HuffmanTree tree = new HuffmanTree(createFrequencyMap(infile));
-       BitInputStream in = new BitInputStream(infile);
-       BitOutputStream out = new BitOutputStream(outfile);
-       out.writeBits(1846, 32);
-       tree.serialize(out);
-       tree.encode(in, out);
-       in.close();
-       out.close();
-   }
-   
-   public static void main(String args[]) throws IOException{
-       encode("input.txt", "output");
-       decode("output", "input.txt");
-   }
-   
-    
+	
+	/**
+	 * 
+	 * @param infile a String
+	 * @param outfile a String
+	 * @throws IOException
+	 */
+	public static void decode(String infile, String outfile) throws IOException {
+		BitInputStream in = new BitInputStream(infile);
+		BitOutputStream out = new BitOutputStream(outfile);
+		int magic = in.readBits(32);
+		if (magic != 1846) {
+			throw new IllegalArgumentException("Not a .grin file");
+		}
+		HuffmanTree tree = new HuffmanTree(in);
+		tree.decode(in, out);
+		in.close();
+		out.close();
+	}
+
+	/**
+	 * 
+	 * @param file a String
+	 * @return a Map
+	 * @throws IOException
+	 */
+	public static Map<Short, Integer> createFrequencyMap(String file) throws IOException {
+		Map<Short, Integer> m = new HashMap<>();
+		BitInputStream in = new BitInputStream(file);
+		while (in.hasBits()) {
+			int temp = in.readBits(8);
+			if (m.containsKey((short) temp)) {
+				m.put((short) temp, m.get((short) temp) + 1);
+			} else {
+				m.put((short) temp, 1);
+			}
+		}
+		in.close();
+		return m;
+	}
+
+	/**
+	 * 
+	 * @param infile a String
+	 * @param outfile a String
+	 * @throws IOException
+	 */
+	public static void encode(String infile, String outfile) throws IOException {
+		HuffmanTree tree = new HuffmanTree(createFrequencyMap(infile));
+		BitInputStream in = new BitInputStream(infile);
+		BitOutputStream out = new BitOutputStream(outfile);
+		out.writeBits(1846, 32);
+		tree.serialize(out);
+		tree.encode(in, out);
+		in.close();
+		out.close();
+	}
+
+	public static void main(String args[]) throws IOException {
+		if (args[0].equals("encode")) {
+			encode(args[1], args[2]);
+		} else if (args[0].equals("decode")) {
+			decode(args[1], args[2]);
+		} else {
+			throw new IllegalArgumentException("Invalid arguments given; either encode or decode");
+		}
+	}
+
 }
--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -4,145 +4,179 @@ import java.util.Map;
 import java.util.PriorityQueue;
 
 public class HuffmanTree {
-    public class Node implements Comparable<Node> {
-        private short val;
-        private int freq;
-        private Node left;
-        private Node right;
+	public static class Node implements Comparable<Node> {
+		private short val;
+		private int freq;
+		private Node left;
+		private Node right;
 
-        public Node(short val, int freq) {
-            this.val = val;
-            this.freq = freq;
-            left = null;
-            right = null;
-        }
+		public Node(short val, int freq) {
+			this.val = val;
+			this.freq = freq;
+			left = null;
+			right = null;
+		}
 
-        public Node(short val) {
-            this.val = val;
-            left = null;
-            right = null;
-        }
+		public Node(short val) {
+			this.val = val;
+			left = null;
+			right = null;
+		}
 
-        public Node(Node left, Node right) {
-            this.freq = left.freq + right.freq;
-            this.left = left;
-            this.right = right;
-        }
+		public Node(Node left, Node right) {
+			this.freq = left.freq + right.freq;
+			this.left = left;
+			this.right = right;
+		}
 
-        public int compareTo(Node n) {
-            if (this.freq < n.freq) {
-                return -1;
-            } else if (this.freq > n.freq) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
+		/**
+		 * 
+		 * @param n a Node
+		 * @return an Integer
+		 */
+		public int compareTo(Node n) {
+			if (this.freq < n.freq) {
+				return -1;
+			} else if (this.freq > n.freq) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
 
-    }
+	}
 
-    private Node root;
-    private Map<Short, String> encodes;
+	private Node root;
+	private Map<Short, String> encodes;
 
-    public HuffmanTree(Map<Short, Integer> m) {
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        Iterator<Short> iter = m.keySet().iterator();
-        while (iter.hasNext()) {
-            short key = iter.next();
-            queue.add(new Node(key, m.get(key)));
-        }
-        queue.add(new Node((short) 256, 1));
+	public HuffmanTree(Map<Short, Integer> m) {
+		PriorityQueue<Node> queue = new PriorityQueue<>();
+		Iterator<Short> iter = m.keySet().iterator();
+		while (iter.hasNext()) {
+			short key = iter.next();
+			queue.add(new Node(key, m.get(key)));
+		}
+		queue.add(new Node((short) 256, 1));
 
-        while (queue.size() > 1) {
-            root = new Node(queue.poll(), queue.poll());
-            queue.add(root);
-        }
-        this.encodes = new HashMap<Short, String>();
-        buildEncodes(this.root, "");
-    }
+		while (queue.size() > 1) {
+			root = new Node(queue.poll(), queue.poll());
+			queue.add(root);
+		}
+		this.encodes = new HashMap<Short, String>();
+		buildEncodes(this.root, "");
+	}
 
-    public HuffmanTree(BitInputStream in) {
-        root = HuffmanTreeH(in);
-    }
+	public HuffmanTree(BitInputStream in) {
+		root = HuffmanTreeH(in);
+	}
 
-    public Node HuffmanTreeH(BitInputStream in) {
-        int temp = in.readBit();
-        if (temp == 1) {
-            return new Node(HuffmanTreeH(in), HuffmanTreeH(in));
-        } else if (temp == 0) {
-            return new Node((short) in.readBits(9));
-        } else {
-            throw new IllegalArgumentException();
-        }
-    }
+	/**
+	 * 
+	 * @param in a BitInputStream
+	 * @return a Node
+	 */
+	public Node HuffmanTreeH(BitInputStream in) {
+		int temp = in.readBit();
+		if (temp == 1) {
+			return new Node(HuffmanTreeH(in), HuffmanTreeH(in));
+		} else if (temp == 0) {
+			return new Node((short) in.readBits(9));
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
 
-    public void serialize(BitOutputStream out) {
-        serializeH(out, root);
-    }
+	/**
+	 * 
+	 * @param out a BitOutputStream
+	 */
+	public void serialize(BitOutputStream out) {
+		serializeH(out, root);
+	}
 
-    public void serializeH(BitOutputStream out, Node cur) {
-        if (cur.left == null && cur.right == null) {
-            out.writeBit(0);
-            out.writeBits((int) cur.val, 9);
-        } else {
-            out.writeBit(1);
-            serializeH(out, cur.left);
-            serializeH(out, cur.right);
-        }
-    }
+	/**
+	 * 
+	 * @param out a BitOutputStream
+	 * @param cur a Node
+	 */
+	public void serializeH(BitOutputStream out, Node cur) {
+		if (cur.left == null && cur.right == null) {
+			out.writeBit(0);
+			out.writeBits((int) cur.val, 9);
+		} else {
+			out.writeBit(1);
+			serializeH(out, cur.left);
+			serializeH(out, cur.right);
+		}
+	}
 
-    private void buildEncodes(Node node, String str) {
-        if (node.left == null && node.right == null) {
-            encodes.put(node.val, str);
-            str = "";
-        }
-        if (node.left != null) {
-            buildEncodes(node.left, str + "0");
-        }
-        if (node.right != null) {
-            buildEncodes(node.right, str + "1");
-        }
-    }
+	/**
+	 * 
+	 * @param node a Node
+	 * @param str a String
+	 */
+	private void buildEncodes(Node node, String str) {
+		if (node.left == null && node.right == null) {
+			encodes.put(node.val, str);
+			str = "";
+		}
+		if (node.left != null) {
+			buildEncodes(node.left, str + "0");
+		}
+		if (node.right != null) {
+			buildEncodes(node.right, str + "1");
+		}
+	}
 
-    public void encode(BitInputStream in, BitOutputStream out) {
-        short value = (short) in.readBits(8);
-        while (value != -1) {
-            String treePath = this.encodes.get((short) value);
-            for (int i = 0; i < treePath.length(); i++) {
-                if (treePath.charAt(i) == '1') {
-                    out.writeBit(1);
-                } else if (treePath.charAt(i) == '0') {
-                    out.writeBit(0);
-                }
-            }
-            value = (short) in.readBits(8);
-        }
-        String eof = encodes.get((short) 256);
-        for (int i = 0; i < eof.length(); i++) {
-            if (eof.charAt(i) == '1') {
-                out.writeBit(1);
-            } else if (eof.charAt(i) == '0') {
-                out.writeBit(0);
-            }
-        }
-    }
+	/**
+	 * 
+	 * @param in a BitInputStream
+	 * @param out a BitOutputStream
+	 */
+	public void encode(BitInputStream in, BitOutputStream out) {
+		short value = (short) in.readBits(8);
+		while (value != -1) {
+			String treePath = this.encodes.get((short) value);
+			for (int i = 0; i < treePath.length(); i++) {
+				if (treePath.charAt(i) == '1') {
+					out.writeBit(1);
+				} else if (treePath.charAt(i) == '0') {
+					out.writeBit(0);
+				}
+			}
+			value = (short) in.readBits(8);
+		}
+		String eof = encodes.get((short) 256);
+		for (int i = 0; i < eof.length(); i++) {
+			if (eof.charAt(i) == '1') {
+				out.writeBit(1);
+			} else if (eof.charAt(i) == '0') {
+				out.writeBit(0);
+			}
+		}
+	}
 
-    public void decode(BitInputStream in, BitOutputStream out) {
-        while (in.hasBits()) {
-            Node node = this.root;
-            while (node.left != null || node.right != null) {
-                if (in.readBit() == 0) {
-                    node = node.left;
-                } else {
-                    node = node.right;
-                }
-            }
-            Short sh = (short) (int) node.val;
-            if (sh == 256) {
-                return;
-            } else {
-                out.writeBits(sh, 8);
-            }
-        }
-    }
+	/**
+	 * 
+	 * @param in a BitInputStream
+	 * @param out a BitOutputStream
+	 */
+	public void decode(BitInputStream in, BitOutputStream out) {
+		while (in.hasBits()) {
+			Node node = this.root;
+			while (node.left != null || node.right != null) {
+				if (in.readBit() == 0) {
+					node = node.left;
+				} else {
+					node = node.right;
+				}
+			}
+			Short sh = (short) (int) node.val;
+			if (sh == 256) {
+				return;
+			} else {
+				out.writeBits(sh, 8);
+			}
+		}
+	}
 }
